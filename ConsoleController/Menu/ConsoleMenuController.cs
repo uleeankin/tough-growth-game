ee8d3a91477088cf521d62b09cf2b5b573dffca6
uleeankin <sokolowa.yu.a@gmail.com>
2022-11-11 15:41:10 +0300

    init console application

---
+++ ConsoleController/Menu/ConsoleMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controller.Menu;
+
+namespace ConsoleController.Menu
+{
+    public class ConsoleMenuController : MenuController
+    {
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
