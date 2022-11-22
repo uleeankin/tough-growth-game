f56520db33943fef0cedb8fd0cda6fd3fcc6e7a3
uleeankin <sokolowa.yu.a@gmail.com>
2022-11-22 18:16:03 +0300

    add new main menu

---
+++ ConsoleView/Items/ConsoleViewPassiveItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View.Items;
+using Model.Items;
+
+namespace ConsoleView.Items
+{
+    public class ConsoleViewPassiveItem : ViewPassiveItem
+    {
+
+        private Utils.CastomOutput _output = new Utils.CastomOutput();
+
+        public ConsoleViewPassiveItem(PassiveItem parPassiveItem) : base(parPassiveItem)
+        {
+        }
+
+        public override void Draw()
+        {
+            
+
+        }
+    }
+}
