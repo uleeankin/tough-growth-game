e35c5441446e28fc73d3f8485f0611cc92593ec3
uleeankin <sokolowa.yu.a@gmail.com>
2022-11-22 19:48:23 +0300

    add console records and info

---
+++ ConsoleController/Menu/ConsoleRecordsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controller.Menu;
+using View.Menu;
+
+namespace ConsoleController.Menu
+{
+    public class ConsoleRecordsController : RecordsController
+    {
+        private ViewRecords _viewRecords = null;
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Stop()
+        {
+            
+        }
+    }
+}
