29d39ee289866fb6ede141c8253f02a5340ae263
uleeankin <sokolowa.yu.a@gmail.com>
2022-12-18 17:21:01 +0300

    init base records

--- Model/Menu/Records.cs
+++ Model/Menu/Records.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Model.Items;
 using Model.Enums;
+using Model.Utils;
 
 namespace Model.Menu
 {
@@ -12,9 +13,40 @@ namespace Model.Menu
     {
         public Records() : base()
         {
+            this.GetRecords();
             this.AddControlItem(new ControlItem((int)ControlItemCode.MainMenu,
                                         Properties.Resources.MainMenuControlItem));
             this.FocusItemById((int)ControlItemCode.MainMenu);
         }
+
+        public void GetRecords()
+        {
+            this.DeletePassiveItems();
+            List<Tuple<string, int>> recordsData 
+                = FileIO.RecordsFileReader(
+                    Properties.Resources.RecordsFileName)
+                        .OrderBy(x => x.Item2).ToList();
+
+            if (recordsData.Count != 0)
+            {
+                if (recordsData.Count > 10)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        this.AddPassiveItem(
+                            new PassiveItem(
+                                $"{i + 1} место. {recordsData[i].Item1} - {recordsData[i].Item2}"));
+                    }
+                } else
+                {
+                    for (int i = 0; i < recordsData.Count; i++)
+                    {
+                        this.AddPassiveItem(
+                            new PassiveItem(
+                                $"{i + 1} место. {recordsData[i].Item1} - {recordsData[i].Item2}"));
+                    }
+                }
+            }
+         }
     }
 }
