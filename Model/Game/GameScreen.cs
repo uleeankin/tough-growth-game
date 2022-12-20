40574c7182b4ee16efd6a2302d25e583b8354bc5
uleeankin <sokolowa.yu.a@gmail.com>
2022-12-20 17:25:28 +0300

    full game realisation

--- Model/Game/GameScreen.cs
+++ Model/Game/GameScreen.cs
@@ -8,23 +8,39 @@ using Model.Enums;
 using Model.Utils;
 using System.Threading;
 using System.Diagnostics;
+using System.Timers;
+using Barrier = Model.Game.GameObjects.Barrier;
 
 namespace Model.Game
 {
     public class GameScreen : Screen
     {
+        private const double PLAYER_SPEED = 200;
+        private const double RECTANGLE_SPEED = 100;
+        private const double SHORT_BARRIER_SPEED = 150;
+        private const double LONG_BARRIER_SPEED = 50;
+        private const double ARROW_BARRIER_SPEED = 70;
+
         public delegate void dNeedRedraw();
         public event dNeedRedraw NeedRedraw = null;
 
         public delegate void dEndGame();
         public event dEndGame EndGame = null;
 
-        private bool _isNeedStop = false;
-        private double _objectsSpeed = 200;
-        private double _rectangleSpeed = 100;
-        private double _timeCoefficient = 0;
+        public delegate void dOnBarriersChange();
+        public event dOnBarriersChange OnBarriersChange = null;
 
-        private List<GameObject> _gameObjects 
+        private bool _isNeedStop = false;
+        private double _timeCoefficient = 0;
+        private volatile int _inactiveObjectsNumber = 0;
+        private volatile int _eatenObjectsNumber = 0;
+
+        private System.Timers.Timer _shortBarrierTimer = new System.Timers.Timer(2000);
+        private System.Timers.Timer _longBarrierTimer = new System.Timers.Timer(3000);
+
+        private volatile List<Barrier> _barriers = new List<Barrier>();
+
+        private List<GameObject> _gameObjects
             = new List<GameObject>();
 
         private Dictionary<int, List<GameObject>> _levelObjects
@@ -34,6 +50,18 @@ namespace Model.Game
 
         public double ScreenHeight { get; set; }
         public double ScreenWidth { get; set; }
+
+        public bool HasHexagons { get; set; }
+        public bool HasTriangles { get; set; }
+
+        public List<Barrier> Barriers
+        {
+            get
+            {
+                return _barriers;
+            }
+        }
+
         public int Level
         {
             get
@@ -68,6 +96,7 @@ namespace Model.Game
         {
             _levelObjects = LevelsParser.GetLevels(10);
             Init();
+            InitTimers();
         }
 
         private void Init()
@@ -84,8 +113,37 @@ namespace Model.Game
                 });
                 ((PermanentSquare)_gameObjects[(int)GameObjectTypes.PERMANENT_SQUARE])
                                     .NeedNewPosition += SetPermanentSquareCoordinates;
+                _eatenObjectsNumber = 0;
+                _inactiveObjectsNumber = _gameObjects.Count - 2;
+                _barriers.Clear();
+                if (this[(int)GameObjectTypes.HEXAGON].Count != 0)
+                {
+                    HasHexagons = true;
+                }
+                else
+                {
+                    HasHexagons = false;
+                }
+
+                if (this[(int)GameObjectTypes.TRIANGLE].Count != 0)
+                {
+                    HasTriangles = true;
+                }
+                else
+                {
+                    HasTriangles = false;
+                }
             }
-            
+
+        }
+
+        private void InitTimers()
+        {
+            _shortBarrierTimer.AutoReset = true;
+            _shortBarrierTimer.Elapsed += OnShortShotsTimerEvent;
+            _longBarrierTimer.AutoReset = true;
+            _longBarrierTimer.Elapsed += OnLongShotsTimerEvent;
+
         }
 
         private void SetPermanentSquareCoordinates()
@@ -129,6 +187,8 @@ namespace Model.Game
 
         public void StartGame()
         {
+            _shortBarrierTimer.Enabled = true;
+            _longBarrierTimer.Enabled = true;
             _isNeedStop = false;
             if (_level == 1 || _level > 10)
             {
@@ -137,7 +197,7 @@ namespace Model.Game
             }
 
             StartNewLevel();
-            
+
             new Thread(() =>
             {
                 Stopwatch timer = new Stopwatch();
@@ -154,25 +214,60 @@ namespace Model.Game
 
         public void StopGame()
         {
+            _shortBarrierTimer.Enabled = false;
+            _longBarrierTimer.Enabled = false;
             _isNeedStop = true;
         }
 
         private void Move()
         {
-            ((GameSquare)_gameObjects[(int)GameObjectTypes.GAME_SQUARE])
-                .MoveByStep(_objectsSpeed * _timeCoefficient, ScreenHeight, ScreenWidth);
-            this[(int)GameObjectTypes.RECTANGLE].ForEach(x => {
-                ((Rectangle)x).MoveByStep(_rectangleSpeed * _timeCoefficient);
+
+            GameSquare gameSquare = ((GameSquare)_gameObjects[(int)GameObjectTypes.GAME_SQUARE]);
+            gameSquare.MoveByStep(PLAYER_SPEED * _timeCoefficient, ScreenHeight, ScreenWidth);
+
+            Parallel.ForEach(GameObjects, elGameObject => {
+                if (elGameObject.ID == GameObjectTypes.RECTANGLE)
+                {
+                    ((Rectangle)elGameObject).MoveByStep(RECTANGLE_SPEED * _timeCoefficient);
+                }
             });
+
+            for (int i = 0; i < Barriers.Count; i++)
+            {
+                Barrier elBarrier = Barriers[i];
+                if (elBarrier.Parent.ID == GameObjectTypes.CIRCLE)
+                {
+                    elBarrier.EndX = gameSquare.X;
+                    elBarrier.EndY = gameSquare.Y;
+                }
+
+                if (elBarrier.ID == BarrierType.SHORT_SHOT
+                    || elBarrier.ID == BarrierType.LONG_SHOT)
+                {
+                    if (elBarrier.X < 0
+                        || elBarrier.Y < 0
+                        || elBarrier.X > ScreenWidth
+                        || elBarrier.Y > ScreenHeight)
+                    {
+                        StopBarrier(elBarrier);
+                    }
+                }
+
+                elBarrier.MoveByStep(GetBarrierSpeed(elBarrier.ID) * _timeCoefficient);
+            }
+
             CheckIntersections();
+            CheckBarriersIntersections();
+            DeleteInactiveBarrier();
         }
 
         private void CheckIntersections()
         {
             GameObject gameSquare = GameObjects[(int)GameObjectTypes.GAME_SQUARE];
-            foreach (GameObject elGameObject in GameObjects)
+            Parallel.ForEach(GameObjects, elGameObject =>
             {
-                if (elGameObject.ID != GameObjectTypes.GAME_SQUARE)
+                if (elGameObject.ID != GameObjectTypes.GAME_SQUARE
+                    && elGameObject.State != GameObjectsStates.INACTIVE)
                 {
                     if (gameSquare.Area >= elGameObject.Area)
                     {
@@ -180,33 +275,38 @@ namespace Model.Game
                         {
                             if (elGameObject.State == GameObjectsStates.FOOD)
                             {
-                                if (_gameObjects.FindAll(
-                                    (x) => x.State == GameObjectsStates.EATEN)
-                                    .Count == GameObjects.Length - 2)
+                                if (_eatenObjectsNumber == GameObjects.Length - 2
+                                        && elGameObject.ID == GameObjectTypes.PERMANENT_SQUARE)
                                 {
                                     Level++;
                                     StartNewLevel();
-                                    break;
+                                    return;
                                 }
 
-                                gameSquare.Area += (elGameObject.Area / (Level * 1.5));
+                                gameSquare.Area += elGameObject.Area;
                                 elGameObject.State = GameObjectsStates.EATEN;
 
-                                if (_gameObjects.FindAll(
-                                    (x) => x.State == GameObjectsStates.INACTIVE)
-                                    .Count == GameObjects.Length - 2)
+                                if (_barriers.Count != 0)
                                 {
-                                    _gameObjects.ForEach((x) =>
-                                    {
-                                        if (x.ID == GameObjectTypes.RECTANGLE)
-                                        {
-                                            ((Rectangle)x).IsActiveMotion = true;
-                                        }
-                                        SetNewState(x,
-                                            GameObjectsStates.INACTIVE, GameObjectsStates.BARRIER);
-                                    });
+                                    StopBarrier(elGameObject);
                                 }
 
+                                if (elGameObject.ID != GameObjectTypes.PERMANENT_SQUARE)
+                                {
+                                    _eatenObjectsNumber++;
+                                }
+
+                                if (_inactiveObjectsNumber == GameObjects.Length - 2)
+                                {
+                                    _gameObjects.ForEach((elObject) =>
+                                    {
+
+                                        StartBarriers(elObject, gameSquare);
+                                        SetNewState(elObject,
+                                            GameObjectsStates.INACTIVE, GameObjectsStates.BARRIER);
+                                    });
+                                    _inactiveObjectsNumber = 0;
+                                }
                             }
 
                         }
@@ -219,13 +319,89 @@ namespace Model.Game
                         {
                             if (elGameObject.State == GameObjectsStates.BARRIER)
                             {
-                                Deaths++;
-                                Level = Level < 8 ? Level : Level - 1;
-                                StartNewLevel();
+                                EndLevel();
                             }
                         }
                     }
                 }
+            });
+        }
+
+        private void CheckBarriersIntersections()
+        {
+            GameObject gameSquare = GameObjects[(int)GameObjectTypes.GAME_SQUARE];
+            for (int i = 0; i < _barriers.Count; i++)
+            {
+                if (_barriers[i].State == GameObjectsStates.BARRIER)
+                {
+                    if (IsIntersection(Barriers[i], gameSquare))
+                    {
+                        EndLevel();
+                    }
+                }
+            }
+        }
+
+        private void EndLevel()
+        {
+            Deaths++;
+            Level = Level < 8 ? Level : Level - 1;
+            StartNewLevel();
+        }
+
+        private double GetBarrierSpeed(BarrierType parBarrierType)
+        {
+            switch (parBarrierType)
+            {
+                case BarrierType.ARROW:
+                    return ARROW_BARRIER_SPEED;
+                case BarrierType.SHORT_SHOT:
+                    return SHORT_BARRIER_SPEED;
+                case BarrierType.LONG_SHOT:
+                    return LONG_BARRIER_SPEED;
+                default:
+                    return 0;
+            }
+        }
+
+        private void OnShortShotsTimerEvent(Object source, ElapsedEventArgs e)
+        {
+            if (HasHexagons)
+            {
+                GameObject gameSquare = GameObjects[(int)GameObjectTypes.GAME_SQUARE];
+                foreach (GameObject elGameObject in GameObjects)
+                {
+                    if (elGameObject.ID == GameObjectTypes.HEXAGON
+                        && (elGameObject.State == GameObjectsStates.BARRIER
+                        || elGameObject.State == GameObjectsStates.FOOD))
+                    {
+                        _barriers.Add(new Barrier(BarrierType.SHORT_SHOT,
+                            gameSquare.X, gameSquare.Y, 10, 10, elGameObject,
+                            ScreenHeight, ScreenWidth));
+                    }
+                }
+                OnBarriersChange?.Invoke();
+            }
+        }
+
+        private void OnLongShotsTimerEvent(Object source, ElapsedEventArgs e)
+        {
+            if (HasTriangles)
+            {
+                GameObject gameSquare = GameObjects[(int)GameObjectTypes.GAME_SQUARE];
+                foreach (GameObject elGameObject in GameObjects)
+                {
+                    if (elGameObject.ID == GameObjectTypes.TRIANGLE
+                        && (elGameObject.State == GameObjectsStates.BARRIER
+                        || elGameObject.State == GameObjectsStates.FOOD))
+                    {
+                        _barriers.Add(new Barrier(BarrierType.LONG_SHOT,
+                            gameSquare.X, gameSquare.Y, 40, 40, elGameObject,
+                            ScreenHeight, ScreenWidth));
+
+                    }
+                }
+                OnBarriersChange?.Invoke();
             }
         }
 
@@ -239,6 +415,46 @@ namespace Model.Game
             }
         }
 
+        private void StartBarriers(GameObject parGameObject, GameObject parGameSquare)
+        {
+            if (parGameObject.ID == GameObjectTypes.RECTANGLE)
+            {
+                ((Rectangle)parGameObject).IsActiveMotion = true;
+            }
+
+            if (parGameObject.ID == GameObjectTypes.CIRCLE)
+            {
+                _barriers.Add(new Barrier(BarrierType.ARROW, parGameObject.X,
+                    parGameSquare.Y, 25, 25, parGameObject));
+                OnBarriersChange?.Invoke();
+            }
+        }
+
+        private void StopBarrier(GameObject parParent)
+        {
+            _barriers.ForEach((elBarrier) =>
+            {
+                if (elBarrier.Parent.Equals(parParent))
+                {
+                    elBarrier.State = GameObjectsStates.INACTIVE;
+                }
+            });
+        }
+
+        private void StopBarrier(Barrier parBarrier)
+        {
+            parBarrier.State = GameObjectsStates.INACTIVE;
+        }
+
+        private void DeleteInactiveBarrier()
+        {
+            if (Barriers.FindAll(elBarrier => elBarrier.State == GameObjectsStates.INACTIVE).Count != 0)
+            {
+                _barriers.Remove(_barriers.Find(elBarrier => elBarrier.State == GameObjectsStates.INACTIVE));
+                OnBarriersChange?.Invoke();
+            }
+        }
+
         private bool IsIntersection(GameObject parGameObject, GameObject parGameSquare)
         {
             return GetXIntersection(parGameObject.X, parGameSquare.X,
@@ -247,11 +463,20 @@ namespace Model.Game
                             parGameSquare.Height / 2, parGameObject.Height / 2);
         }
 
+        private bool IsIntersection(Barrier parBarrier, GameObject parGameSquare)
+        {
+            return GetXIntersection(parBarrier.X, parGameSquare.X,
+                            parGameSquare.Width / 2, parBarrier.Width / 2)
+                        && GetYIntersection(parBarrier.Y, parGameSquare.Y,
+                            parGameSquare.Height / 2, parBarrier.Height / 2);
+        }
+
         private void StartNewLevel()
         {
             ((PermanentSquare)_gameObjects[(int)GameObjectTypes.PERMANENT_SQUARE])
                                 .NeedNewPosition -= SetPermanentSquareCoordinates;
             Init();
+            OnBarriersChange?.Invoke();
             if (Level <= 10)
             {
                 NeedRedraw?.Invoke();
--- View/Game/ViewGame.cs
+++ View/Game/ViewGame.cs
@@ -11,8 +11,20 @@ namespace View.Game
     {
 
         private List<ViewGameObject> _gameObjects = null;
+        private List<ViewBarrier> _barriers = null;
 
         protected List<ViewGameObject> Objects => _gameObjects;
+        protected List<ViewBarrier> Barriers
+        {
+            get
+            {
+                return _barriers;
+            }
+            set
+            {
+                _barriers = value;
+            }
+        }
 
         public int X { get; set; }
         public int Y { get; set; }
@@ -21,32 +33,33 @@ namespace View.Game
 
         protected Model.Game.GameScreen Screen { get; set; }
 
-        public ViewGameObject this[int parId]
-        {
-            get
-            {
-                return _gameObjects.Find((x) => (int)x.Object.ID == parId);
-            }
-        }
-
         public ViewGame(Model.Game.GameScreen parGameScreen)
         {
             Screen = parGameScreen;
             Screen.NeedRedraw += Redraw;
+            Screen.OnBarriersChange += OnBarriersChange;
             _gameObjects = new List<ViewGameObject>();
+            _barriers = new List<ViewBarrier>();
 
             foreach (Model.Game.GameObjects.GameObject elGameObject in parGameScreen.GameObjects)
             {
                 _gameObjects.Add(CreateGameObject(elGameObject));
             }
+            foreach (Model.Game.GameObjects.Barrier elBarrier in parGameScreen.Barriers)
+            {
+                _barriers.Add(CreateBarrier(elBarrier));
+            }
         }
 
         protected abstract void Redraw();
         protected abstract ViewGameObject CreateGameObject(Model.Game.GameObjects.GameObject parGameObject);
+        protected abstract ViewBarrier CreateBarrier(Model.Game.GameObjects.Barrier parBarrier);
+        protected abstract void OnBarriersChange();
 
         protected void ClearObjects()
         {
             _gameObjects = new List<ViewGameObject>();
+            _barriers = new List<ViewBarrier>();
         }
     }
 }
