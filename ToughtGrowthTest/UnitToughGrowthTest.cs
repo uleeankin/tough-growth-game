f3055e3d98b80d00a8591680485030b3214e9360
uleeankin <sokolowa.yu.a@gmail.com>
2022-12-22 19:54:17 +0300

    init tests

---
+++ ToughtGrowthTest/UnitToughGrowthTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToughtGrowthTest
+{
+    [TestClass]
+    public class UnitToughGrowthTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
